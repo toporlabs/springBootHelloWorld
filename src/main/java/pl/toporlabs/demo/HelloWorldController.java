5fcea6dc233fa4696c543faa807d87e6824b5f01
Topor <mastercezary@gmail.com>
2017-08-10 16:42:11 +0200

    hello world

---
+++ src/main/java/pl/toporlabs/demo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package pl.toporlabs.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+}
